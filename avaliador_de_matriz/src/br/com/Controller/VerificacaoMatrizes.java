35cc4be76ef8b21af1163bce8f680e819593e748
Candido <candido.neto@ucsal.edu.br>
2021-09-02 08:58:37 -0300

    Inicializacao

---
+++ avaliador_de_matriz/src/br/com/Controller/VerificacaoMatrizes.java
@@ -0,0 +1,5 @@
+package br.com.Controller;
+
+public class VerificacaoMatrizes {
+    
+}
---
+++ avaliador_de_matriz/src/br/com/TUI/TelaInicial.java
@@ -0,0 +1,13 @@
+package br.com.TUI;
+
+public class TelaInicial {
+
+    public static void main(String[] args) {
+       boasVindas();    
+    }
+
+    public static void boasVindas(){
+        System.out.println("Bem Vindo");
+    }
+
+}
