2a53c5b2c284ec309c99bdb9cd1707c895ad2e80
Candido <candido.neto@ucsal.edu.br>
2021-09-03 11:54:22 -0300

    Primeira Versão Do Programa

--- avaliador_de_matriz/src/br/com/Controller/VerificacaoMatrizes.java
+++ avaliador_de_matriz/src/br/com/Controller/VerificacaoMatrizes.java
@@ -1,5 +1,12 @@
 package br.com.Controller;
 
 public class VerificacaoMatrizes {
-    
+    private VerificacaoMatrizes(){}
+
+    public static boolean verificarSimetricidade(int l, int c) {
+        if(l == c)
+            return true;
+        else
+            return false;
+    }
 }
--- avaliador_de_matriz/src/br/com/TUI/TelaInicial.java
+++ avaliador_de_matriz/src/br/com/TUI/TelaInicial.java
@@ -1,13 +1,37 @@
 package br.com.TUI;
 
-public class TelaInicial {
+import java.util.Scanner;
 
+import br.com.Controller.VerificacaoMatrizes;
+
+public class TelaInicial {
+    private static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
-       boasVindas();    
+       iniciar();    
     }
 
-    public static void boasVindas(){
+    public static void iniciar(){
         System.out.println("Bem Vindo");
+        System.out.println("Insira a linha e a coluna da matriz nessa ordem");
+        System.out.println("Para encerrar o programa digite um valor negativo");
+        int aux = 0;
+        int aux2 = 0;
+        int v1 = 0;
+        int v2 = 0;
+        String result = "";
+
+        while(aux >= 0 ){
+            aux2++;
+            aux = s.nextInt();
+            v1 = aux;
+            if (v1 >= 0){
+                aux = s.nextInt();
+                v2 = aux;
+                if (v2 >= 0)
+                    result += aux2 + " - " + VerificacaoMatrizes.verificarSimetricidade(v1,v2) + "\n";
+            }
+        }
+        System.out.println(result);
     }
 
 }
