c881e0749ad4461e5f5b247a55788bd161ecfd1b
Candido <candido.neto@ucsal.edu.br>
2021-09-03 12:49:30 -0300

    Versão 0.2

--- avaliador_de_matriz/src/br/com/Controller/VerificacaoMatrizes.java
+++ avaliador_de_matriz/src/br/com/Controller/VerificacaoMatrizes.java
@@ -5,22 +5,13 @@ public class VerificacaoMatrizes {
 
     public static boolean verificarSimetricidade(int[][] matriz) {
         int[][] aux = new int [2][2];
-        int validador = 0;
         
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 aux[i][j] = matriz[j][i];
             }
         }
-        
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                if(aux[i][j] == matriz[i][j]){
-                    validador++;
-                }
-            }
-        }
-        if(validador == 4)
+        if(aux[0][0] == matriz[1][1] && aux[1][0] == matriz[0][1])
             return true;
         else
             return false;
